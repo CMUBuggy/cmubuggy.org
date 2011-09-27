c2ba8f906f11707236b820bbbda87deb5a637ac6
Sam Swift <samswift@gmail.com>
2011-09-27 07:58:06 -0400

    somehow it seems that there hasnt been google analytics code in the gallery since the new site launched.  that would explain the drop in page views per visit.  its back in now

--- gallery/themes/cmubuggy/views/page.html.php
+++ gallery/themes/cmubuggy/views/page.html.php
@@ -84,6 +84,18 @@
     <link rel="stylesheet" type="text/css" href="/css/cmubuggy.css" />
     <link rel="stylesheet" type="text/css" href="/css/superfish-menu.css" />
     <link  href="http://fonts.googleapis.com/css?family=Lobster:regular" rel="stylesheet" type="text/css" />
+	<!--Google analytics -->
+	<script type="text/javascript">
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-345857-14']);
+	  _gaq.push(['_trackPageview']);
+	
+	  (function() {
+	    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+	    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+	    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+	</script>
   </head>
   <?php 
 	  	$headline = "Gallery";
