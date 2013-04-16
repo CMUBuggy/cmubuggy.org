86b73d779b14080e22ab8ab14227fdcbd0eec92e
Sam Swift <samswift@gmail.com>
2013-04-16 00:20:07 -0400

    Revert "added a new table for misc variables so that simple site values can be edited through the CMS. current values include the homepage headline and upcoming raceday date"
    This reverts commit b1b511fb3bb0575bcde5cad35eb8b30e7a9cb08f.

--- index.php
+++ index.php
@@ -10,15 +10,7 @@
 		$s = $_GET["s"];
 	}
 	$title = "CMU Buggy Alumni Association";
-	
-	$miscvar = new miscvar();
-	$miscVarList = $miscvar->GetList(array(array("key","=","HomeHeadline")));
-	if(count($miscVarList)>0){
-		$headline = $miscVarList[0]->value;
-	}	 else {
-		$headline = "CMU Buggy";
-	}
-		
+	$headline = "Punxsutawney Phil says ROLL";
 	switch($s){
 		case "about":
 			$title = "About | ".$title;
