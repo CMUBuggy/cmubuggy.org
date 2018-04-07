acd5e568533bc1106624f8e34a90b7360675d50a
Krishan Taylor <krishan.taylor@gmail.com>
2018-04-06 20:51:07 -0400

    remove additional manual wp nav

--- news/wp-content/themes/cmubuggy/index.php
+++ news/wp-content/themes/cmubuggy/index.php
@@ -1,22 +1,6 @@
 <?php
 	get_header();
 ?>
-	<div id="topnav">
-		<ul class="sf-menu">
-			<li><a href="#">Categories</a>
-				<ul>
-					<?php wp_list_cats('sort_column=name&optioncount=0&hierarchical=0'); ?>
-				</ul>
-			</li>
-			<li><a href="#">Archives</a>
-				<ul>
-					<?php wp_get_archives('type=monthly'); ?>
-				</ul>
-			</li>
-			<li><a target="_blank" href="http://feedburner.google.com/fb/a/mailverify?uri=BuggyAlumniAssociationNews&amp;loc=en_US">Subscribe to News by Email</a></li>
-			<li><a href="mailto:news@cmubuggy.org">Submit News</a></li>
-		</ul>
-	</div>
 <?php
 	get_template_part( 'loop', 'index' );
 	get_footer();
