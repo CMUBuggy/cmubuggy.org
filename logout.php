6be9694eeedd3735a35b23b37de53da676e27072
Krishan Taylor <krishan.taylor@gmail.com>
2018-04-16 18:35:07 -0400

    Revert "tmp - cookie hack"
    This reverts commit 4707f19d448775945d5a44403291b0f667d9450f.

--- logout.php
+++ logout.php
@@ -1,14 +1,14 @@
 <?php
 
-setcookie("cmubuggy_auth",				"", time()-3600,"/");
-setcookie('cmubuggy_wikiToken', 		"", time()-3600,"/");
-setcookie('cmubuggy_wikiUserID', 	"", time()-3600,"/");
-setcookie('cmubuggy_wikiUserName', 	"", time()-3600,"/");
-setcookie('cmubuggy_wiki_session', 	"", time()-3600,"/");
-setcookie('phpbb3_3ecw2_u',			"", time()-3600,"/");
-setcookie('phpbb3_3ecw2_k',			"", time()-3600,"/");
-setcookie('phpbb3_3ecw2_sid',			"", time()-3600,"/");
-setcookie('g3sid', 						"", time()-3600,"/");
+setcookie("cmubuggy_auth",				"", time()-3600,"/",".cmubuggy.org");
+setcookie('cmubuggy_wikiToken', 		"", time()-3600,"/",".cmubuggy.org");
+setcookie('cmubuggy_wikiUserID', 	"", time()-3600,"/",".cmubuggy.org");
+setcookie('cmubuggy_wikiUserName', 	"", time()-3600,"/",".cmubuggy.org");
+setcookie('cmubuggy_wiki_session', 	"", time()-3600,"/",".cmubuggy.org");
+setcookie('phpbb3_3ecw2_u',			"", time()-3600,"/",".cmubuggy.org");
+setcookie('phpbb3_3ecw2_k',			"", time()-3600,"/",".cmubuggy.org");
+setcookie('phpbb3_3ecw2_sid',			"", time()-3600,"/",".cmubuggy.org");
+setcookie('g3sid', 						"", time()-3600,"/",".cmubuggy.org");
 
 unset($_COOKIE["cmubuggy_auth"]);
 unset($_COOKIE["phpbb3_3ecw2_u"]);
