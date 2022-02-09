3743a106586d28b6de6201ba997b081e041edc0b
robsiemb <robsiemb@users.noreply.github.com>
2022-02-09 17:52:19 -0500

    Update WP CSS handling. (#70)
    - Proper parameters for wp_enqueue_style
    - Cachebusting notes.

--- news/wp-content/themes/cmubuggy-child/functions.php
+++ news/wp-content/themes/cmubuggy-child/functions.php
@@ -1,7 +1,11 @@
 <?php
 function buggy_theme_enqueue_scripts() {
+	// Note: The "Clear Cache For Me" plugin usurps the versioning control
+	// here, so in order to cachebust a new CSS version, you will need to click the
+	// clear cache button on the wordpress dashboard.
 	wp_enqueue_style( 'cmubuggy',
 		get_stylesheet_directory_uri() . '/style.css',
+		array(),
 		wp_get_theme()->get('Version')
 	);
 }
