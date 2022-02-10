870a78cc88ceafe01e72d51562f89e4a329ac0c3
robsiemb <robsiemb@users.noreply.github.com>
2022-02-10 09:47:23 -0500

    cssjs.inc improvements (#74)
    * Update all applicable JS CDNs to be jsdelivr instead of random ones.
    * Extract the common CSS and JS from cssjs.inc and place them in their own files.  This saves about 1-1.5kB (after compression) on every page load.
    * Also, correct bootstrap to be 4.6 instead of 4.1

---
+++ js/cmubuggy-all.js
@@ -0,0 +1,55 @@
+/* Custom JS included in all pages via the cmubuggy cssjs script
+ *
+ * Remember to update the cachebuster when this file is updated.
+ */
+function changeTab(hash) {
+  hash = hash.replace('#','#tab-');
+  $('[href="' + hash + '"]').tab('show');
+}
+
+$(function () {
+  if (window.location.hash) {
+    changeTab(window.location.hash);
+  }
+});
+
+$(window).on('hashchange', function () {
+  changeTab(window.location.hash);
+});
+
+$(document)
+.on('shown.bs.tab', '.nav-tabs a', function (e) {
+  window.location.hash = e.target.hash.replace('#tab-', '#');
+})
+.on('show.bs.modal', '[id^="youtube-"]', function (e) {
+  var $playerDiv = $('div[id^="player-"]', this);
+  if ($playerDiv.length == 0) return;
+  $(this).data('player', new YT.Player($playerDiv.attr('id'), { videoId: $playerDiv.data('videoId') }));
+})
+.on('hide.bs.modal', '[id^="youtube-"]', function (e) {
+  $(this).data('player').stopVideo();
+});
+
+function onYouTubeIframeAPIReady() {}
+
+// Ensure we scroll any non-tab hash targets to below the header bar.
+$(function() {
+  // Some browsers may require a delay after the DOM is ready before we can successfully scroll,
+  // but it does not appear to be required for modern edge and chrome, at least on our site.
+  //
+  // So, don't wait.
+  //
+  // See: https://stackoverflow.com/questions/4086107/fixed-page-header-overlaps-in-page-anchors#35452468
+  //
+  // setTimeout(doFragmentTargetOffset, 500);
+  doFragmentTargetOffset();
+});
+
+// Actually perform the scroll.
+function doFragmentTargetOffset(){
+  var offset = $(':target').offset();
+  if(offset){
+      var scrollto = offset.top - 60; // minus fixed header height
+      $('html,body').animate({scrollTop:scrollto}, 0);
+  }
+}
