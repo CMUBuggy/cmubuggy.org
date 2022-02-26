85cc339505c0888c03ae1296dc76d91789b911c5
Madison Scott <s.madisonlily@gmail.com>
2022-02-26 10:52:00 -0500

    update gallery_name content, rename photo_slug to photo_id

--- scripts/20220221_smugmug-activity.sql
+++ scripts/20220221_smugmug-activity.sql
@@ -14,9 +14,9 @@ CREATE TABLE IF NOT EXISTS `smugmug_uploads` (
     gallery_url VARCHAR(2083),
     content_url VARCHAR(2083),
     thumbnail_url VARCHAR(2083),
-    gallery_name VARCHAR(300),
-    gallery_slug VARCHAR(150),
-    photo_slug VARCHAR(150),
+    gallery_name VARCHAR(150),
+    gallery_slug VARCHAR(300),
+    photo_id VARCHAR(150),
     created_at datetime,
     INDEX (`created_at` DESC)
 );
