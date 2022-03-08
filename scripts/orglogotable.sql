c513ad0791383c0018538eab5421c8aef69cbf75
Rob Siemborski <robsiemb@gmail.com>
2022-03-08 12:10:15 -0500

    First pass at displaying org logos on org pages.

---
+++ scripts/orglogotable.sql
@@ -0,0 +1,28 @@
+# Create an org logo table based on the images we have.
+#
+# Kept separate from the history database for management purposes, but in practice could be merged.
+
+DROP TABLE IF EXISTS orglogos;
+
+CREATE TABLE IF NOT EXISTS orglogos (
+    orgid VARCHAR(15) PRIMARY KEY NOT NULL,
+    image_url VARCHAR(2083)
+);
+
+START TRANSACTION;
+INSERT INTO orglogos (orgid, image_url) VALUES ('AEP',  '/img/logos/aepi.jpg');
+INSERT INTO orglogos (orgid, image_url) VALUES ('APX',  '/img/logos/apex.svg');
+INSERT INTO orglogos (orgid, image_url) VALUES ('CIA',  '/img/logos/cia.gif');
+INSERT INTO orglogos (orgid, image_url) VALUES ('CMTV', '/img/logos/cmutv.png');
+INSERT INTO orglogos (orgid, image_url) VALUES ('FRI',  '/img/logos/fringe.svg');
+INSERT INTO orglogos (orgid, image_url) VALUES ('KS',   '/img/logos/kapsig.gif');
+INSERT INTO orglogos (orgid, image_url) VALUES ('PKA',  '/img/logos/pika.jpg');
+INSERT INTO orglogos (orgid, image_url) VALUES ('PIO',  '/img/logos/pioneers.png');
+INSERT INTO orglogos (orgid, image_url) VALUES ('SAE',  '/img/logos/sae.png');
+INSERT INTO orglogos (orgid, image_url) VALUES ('SDC',  '/img/logos/sdc.svg');
+INSERT INTO orglogos (orgid, image_url) VALUES ('SEP',  '/img/logos/sigep.png');
+INSERT INTO orglogos (orgid, image_url) VALUES ('SN',   '/img/logos/signu.svg');
+INSERT INTO orglogos (orgid, image_url) VALUES ('SPI',   '/img/logos/spirit.png');
+INSERT INTO orglogos (orgid, image_url) VALUES ('W3VC', '/img/logos/w3vc.svg');
+INSERT INTO orglogos (orgid, image_url) VALUES ('WRCT', '/img/logos/wrct2022.png');
+COMMIT;
