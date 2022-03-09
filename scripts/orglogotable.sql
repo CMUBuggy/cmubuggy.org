1e9ea74fff6d0882ed4a2c070ca0e43afe9f97fd
robsiemb <robsiemb@users.noreply.github.com>
2022-03-09 09:24:11 -0500

    Add CMU EMS & Logo (#112)

--- scripts/orglogotable.sql
+++ scripts/orglogotable.sql
@@ -13,6 +13,7 @@ START TRANSACTION;
 INSERT INTO orglogos (orgid, image_url) VALUES ('AEP',  '/img/logos/aepi.jpg');
 INSERT INTO orglogos (orgid, image_url) VALUES ('APX',  '/img/logos/apex.svg');
 INSERT INTO orglogos (orgid, image_url) VALUES ('ATL',  '/img/logos/atlas.jpg');
+INSERT INTO orglogos (orgid, image_url) VALUES ('CEMS', '/img/logos/cmuems.webp');
 INSERT INTO orglogos (orgid, image_url) VALUES ('CIA',  '/img/logos/cia2022.png');
 INSERT INTO orglogos (orgid, image_url) VALUES ('CMTV', '/img/logos/cmutv.png');
 INSERT INTO orglogos (orgid, image_url) VALUES ('DG',   '/img/logos/dg.png');
