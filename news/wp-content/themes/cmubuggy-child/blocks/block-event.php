726940196b5d0c4a7d784adf88d4b0c7fa24d6fd
Madison Scott <s.madisonlily@gmail.com>
2022-03-16 00:05:59 -0400

    add optional location url and make time optional

--- news/wp-content/themes/cmubuggy-child/blocks/block-event.php
+++ news/wp-content/themes/cmubuggy-child/blocks/block-event.php
@@ -2,7 +2,20 @@
   <div class="card-body">
     <h5 class="card-title"><?php block_field('title'); ?></h5>
     <h6 class="card-subtitle mb-2 text-muted">
-      <?php block_field('location'); ?> @ <?php block_field('time'); ?>
+      <?php
+        $location = block_field('location', false);
+        $locationUrl = block_field('location-url', false);
+        if (!empty($locationUrl)) {
+          echo("<a target=\"_blank\" href=\"".$locationUrl."\">".$location."</a>");
+        } else {
+          echo($location);
+        }
+
+        $time = block_field('time', false);
+        if (!empty($time)) {
+          echo(" @ ".$time);
+        }
+      ?>
     </h6>
     <div class="card-text"><?php block_field('description'); ?></div>
     <?php
