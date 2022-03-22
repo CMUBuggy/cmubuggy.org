66f66ddf3f79c86faa0e4b70aab8d43217e1a1e2
Madison Scott <s.madisonlily@gmail.com>
2022-03-21 23:06:49 -0400

    add optional image to event card (#129)

--- news/wp-content/themes/cmubuggy-child/blocks/block-event.php
+++ news/wp-content/themes/cmubuggy-child/blocks/block-event.php
@@ -1,28 +1,36 @@
-<div class="card mb-3">
+<div class="card event-card mb-3">
   <div class="card-body">
-    <h5 class="card-title"><?php block_field('title'); ?></h5>
-    <h6 class="card-subtitle mb-2 text-muted">
-      <?php
-        $location = block_field('location', false);
-        $locationUrl = block_field('location-url', false);
-        if (!empty($locationUrl)) {
-          echo("<a target=\"_blank\" href=\"".$locationUrl."\">".$location."</a>");
-        } else {
-          echo($location);
-        }
+    <div class="event-card-text flex-grow-1">
+      <h5 class="card-title"><?php block_field('title'); ?></h5>
+      <h6 class="card-subtitle mb-2 text-muted">
+        <?php
+          $location = block_field('location', false);
+          $locationUrl = block_field('location-url', false);
+          if (!empty($locationUrl)) {
+            echo("<a target='_blank' href='".$locationUrl."'>".$location."</a>");
+          } else {
+            echo($location);
+          }
 
-        $time = block_field('time', false);
-        if (!empty($time)) {
-          echo(" @ ".$time);
+          $time = block_field('time', false);
+          if (!empty($time)) {
+            echo(" @ ".$time);
+          }
+        ?>
+      </h6>
+      <div class="card-text"><?php block_field('description'); ?></div>
+      <?php
+        $buttonUrl = block_field('button-url', false);
+        if (!empty($buttonUrl)) {
+          $buttonText = block_field('button-text', false);
+          echo("<a target='_blank' href='".$buttonUrl."' class='btn btn-primary'>".$buttonText."</a>");
         }
       ?>
-    </h6>
-    <div class="card-text"><?php block_field('description'); ?></div>
+    </div>
     <?php
-      $buttonUrl = block_field('button-url', false);
-      if (!empty($buttonUrl)) {
-        $buttonText = block_field('button-text', false);
-        echo("<a target=\"_blank\" href=\"".$buttonUrl."\" class=\"btn btn-primary\">".$buttonText."</a>");
+      $imageUrl = block_field('image-url', false);
+      if (!empty($imageUrl)) {
+        echo("<div class='event-card-image'><img src='".$imageUrl."' class='rounded'></div>");
       }
     ?>
   </div>
