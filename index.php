c24aced2964c52f62b83396f7b354023ec0de1e0
robsiemb <robsiemb@users.noreply.github.com>
2022-03-23 07:11:37 -0400

    Revise & Expand OpenGraph Support. (#135)
    * Refactor opengraph data handling & mildly expand coverage.
    * Add simple OpenGraph support for raceday pages.

--- index.php
+++ index.php
@@ -10,19 +10,34 @@
   }
   $title = "CMU Buggy Alumni Association";
 
-  $OPENGRAPH_DATA = "";
+  $OGMAP = array(
+    "og:type" => "website",
+    "og:site_name" => "CMU Buggy Alumni Association"
+    // TODO: Default "og:url" (apparently facebook cannot render og:image without it?)
+  );
+
   switch($s){
     case "history":
       include_once("./content/history/opengraph/opengraphdata.inc");
-      $OPENGRAPH_DATA = getHistoryOpenGraphContent();
+      $OGMAP = getHistoryOpenGraphContent($OGMAP);
       $title = "History | ".$title;
       break;
     case "search":
       $title = "Search Results | ".$title;
       break;
     case "raceday":
+      include_once("./content/raceday/opengraph/opengraphdata.inc");
+      $OGMAP = getRacedayOpenGraphContent($OGMAP);
       $title = "Raceday | ".$title;
       break;
+  }
+
+  // If we haven't yet found a specific opengraph title, use <title>.
+  if (!isset($OGMAP["og:title"])) {
+    $OGMAP["og:title"] = $title;
+    if ($OGMAP["og:site_name"] == $OGMAP["og:title"]) {
+      unset($OGMAP["og:site_name"]);
+    }
   }
 
   if(empty($s)){
@@ -41,12 +56,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1, viewport-fit=cover">
   <meta name="google-site-verification" content="GXsMGGkXYJADa-Rw8I0azRbCk_ILRSXWwTkiHODCBrw" />
   <title><?php echo($title); ?></title>
+  <!-- OpenGraph Metadata -->
 <?php
-  echo($OPENGRAPH_DATA);
-  include_once(ROOT_DIR."/content/cssjs.inc");
-  if (!empty($OEMBED_LINK)) {
-    echo("my oembed is: ". $OEMBED_LINK);
+  foreach ($OGMAP as $key => $value) {
+    echo("  <meta property=\"".$key."\" content=\"".$value."\" />\n");
   }
+  include_once(ROOT_DIR."/content/cssjs.inc");
 ?>
 </head>
 <?php
