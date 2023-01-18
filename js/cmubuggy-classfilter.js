9403ce6007246642b6b99b5df41b22e15c2ba7f4
Rob Siemborski <robsiemb@gmail.com>
2023-01-18 14:40:48 -0500

    Add dynamic filters to views where we have mixed classes.
    This is the org race history and the raceday list view.
    Also add zebra striping where it can help show the different years.

---
+++ js/cmubuggy-classfilter.js
@@ -0,0 +1,29 @@
+// How to use this:
+//
+// Create radio buttons on the page named "classFilter" with values
+// of "ALL" and then one for each CSS class name of the buggy class
+// rows (e.g. "mensRow", "womensRow", etc.)
+//
+// In the table, tag every _row_ with the "classRow" class, as well
+// as the appropriate CSS class for the buggy classes from above.
+// Obviously, the header row will lack any CSS class.
+//
+// If you are parity-coloring the table using table-secondary, and you
+// need that to be removed on the filtered views, also tag those rows
+// with table-parity so it can be disabled/enabled when the views change.
+//
+// Finally, if you have a buggy class column that you wish to hide,
+// tag all that column's _cells_, as well as the header cell, with the
+// "classCol" CSS class.
+
+$("input[type=radio][name=classFilter]").on("change", function() {
+    console.log("hi2");
+  if (this.value == "ALL") {
+    $(".classRow,.classCol").show();
+    $(".classRow.year-parity").addClass("table-secondary");
+  } else {
+    $(".classRow,.classCol").hide();
+    $(".classRow.year-parity").removeClass("table-secondary");
+    $("."+this.value).show();
+  }
+})
