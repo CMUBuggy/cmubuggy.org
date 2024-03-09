dae4ac25f423c0bf799d323e6a54d14fad7fdff3
Madison Scott <s.madisonlily@gmail.com>
2024-03-09 12:50:49 -0500

    refactor masthead into separate component

--- news/wp-content/themes/cmubuggy-child/footer.php
+++ news/wp-content/themes/cmubuggy-child/footer.php
@@ -1,11 +1,13 @@
-			<?php if ( ! is_page_template( 'blank-page.php' ) && ! is_page_template( 'blank-page-with-container.php' )) { ?>
+			<?php
+				if (!is_page_template('blank-page.php') && !is_page_template('blank-page-with-container.php')) {
+			?>
 				</div>
 			<?php
 				}
 			?>
-		</div>
-	</div>
-</div>
+		</div> <!-- col -->
+	</div> <!-- row -->
+</div> <!-- container -->
 
 <?php include_once("../content/footer.inc"); ?>
 <?php wp_footer(); ?>
