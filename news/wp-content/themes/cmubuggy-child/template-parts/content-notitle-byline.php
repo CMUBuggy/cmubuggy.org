3d423767b9ca9f46f580070902dff68176a74677
robsiemb <robsiemb@users.noreply.github.com>
2024-03-18 15:09:53 -0400

    Put back byline and post footer on posts. (#292)

---
+++ news/wp-content/themes/cmubuggy-child/template-parts/content-notitle-byline.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * Template part for displaying posts
+ *
+ * @link https://codex.wordpress.org/Template_Hierarchy
+ *
+ * @package WP_Bootstrap_Starter
+ */
+
+?>
+
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<div class="post-thumbnail">
+		<?php the_post_thumbnail(); ?>
+	</div>
+	<header class="entry-header">
+		<?php
+		if ( 'post' === get_post_type() ) : ?>
+		<div class="entry-meta">
+			<?php wp_bootstrap_starter_posted_on(); ?>
+		</div><!-- .entry-meta -->
+		<?php
+		endif; ?>
+	</header><!-- .entry-header -->
+	<div class="entry-content">
+		<?php
+        if ( is_single() ) :
+			the_content();
+        else :
+            the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'wp-bootstrap-starter' ) );
+        endif;
+
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'wp-bootstrap-starter' ),
+				'after'  => '</div>',
+			) );
+		?>
+	</div><!-- .entry-content -->
+
+	<footer class="entry-footer">
+		<?php wp_bootstrap_starter_entry_footer(); ?>
+	</footer><!-- .entry-footer -->
+</article><!-- #post-## -->
